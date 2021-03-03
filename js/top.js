12da832d44e8a9cab185bf4ae38e39c3fb731fe9
VIETLONG_VN <vvlong@b-create.jp>
2021-03-03 14:41:34 +0700

    updated

--- js/top.js
+++ js/top.js
@@ -1,51 +1,64 @@
-$(document).ready(function () {
+$(document).ready(function() {
     "use strict";
-    $(".hamburger").click(function (e) {
+    $(".hamburger").click(function(e) {
         e.preventDefault();
         $(this).toggleClass('active');
-        $(".menu_overlay").toggleClass("opened");
+        $('#toggle-menu').toggleClass('active');
+        $('.menu').toggleClass('active');
+        $('.sub-menu').hide();
+
     });
-    
-//    $('.slider').slick({
-//        slidesToShow: 1,
-//        infinite: true,
-//        dots: true,
-//        arrows: true,
-//        touchMove: true,
-//        pauseOnHover: false,
-//        autoplay: true,
-//        autoplaySpeed: 3000,
-//        fade: true,
-//        cssEase: 'linear',
-//        responsive: [{
-//            breakpoint: 751,
-//            settings: {
-//                dots: false,
-//                arrows: false,
-//            }
-//    }]
-//    });
-    
-    //  Splitting();
-    //  ScrollOut({
-    //    onShown: function (el) {
-    //      el.classList.add("animated");
-    //    },
-    //    targets: '[data-splitting]',
-    //    once: true
-    //  });
-    //
-    //  if ($(window).width() > 750) {
-    //    new WOW().init();
-    //  }
-    //
-    //  if ($(window).width() > 750) {
-    //    $(window).scroll(function () {
-    //      if ($(this).scrollTop() > 600) {
-    //        $(".side_bnr").addClass('side_bnr_fixed');
-    //      } else {
-    //        $(".side_bnr").removeClass('side_bnr_fixed');
-    //      }
-    //    });
-    //  }
+
+
+    loading();
+    set_slide();
+
+    function loading() {
+        var $loading = $('#loading');
+
+        setTimeout(function() {
+            $loading.addClass('loading-hide');
+        }, 2000);
+    }
+
 });
+
+function loading() {
+    var $loading = $('#loading');
+
+    setTimeout(function() {
+        $loading.addClass('loading-hide');
+    }, 2000);
+}
+
+function set_slide() {
+    $('.mainvisual')
+        .on('init', function() {
+            if ($('.slide').length > 1) {
+                $('.slick-slide[data-slick-index="0"]').addClass('moving');
+            }
+        })
+        .slick({
+            autoplay: true,
+            speed: 5000,
+            autoplaySpeed: 5000,
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            draggable: false,
+            slidesToShow: 1,
+            dots: false,
+            arrows: false,
+            infinite: true,
+            fade: true,
+        })
+        .on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            if ($('.slide').length > 1) {
+                var slide_num = $('.slide').length;
+                $('.slide[data-slick-index="' + (currentSlide - 1) + '"]').removeClass('moving');
+                $('.slide[data-slick-index="' + nextSlide + '"]').addClass('moving');
+                if (currentSlide == 0) {
+                    $('.slide[data-slick-index="' + (slide_num - 1) + '"]').removeClass('moving');
+                }
+            }
+        });
+}
